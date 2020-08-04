b64ceb998b5d666e47f62342122e9fc4c4400069
Sean Ferguson <sean@sgnl24.com>
2020-08-04 12:39:24 -0400

    hide native array extensions in enumeration, add intersect

--- src/native-extensions.js
+++ src/native-extensions.js
@@ -1,35 +1,62 @@
-Array.prototype.diff = function(...args) {
-    return this.filter(val => {
-        for (let i = 0; i < args.length; i++) {
-            if (args[i].includes(val)) {
-                return false;
+Object.defineProperty(Array.prototype, 'diff', {
+    enumerable: false,
+    value: function(...args) {
+        return this.filter(val => {
+            for (let i = 0; i < args.length; i++) {
+                if (args[i].includes(val)) {
+                    return false;
+                }
             }
-        }
-        
-        return true;
-    });
-}
+            
+            return true;
+        });
+    }
+});
 
-Array.prototype.remove = function(element) {
-    const index = this.indexOf(element);
-    if (index >= 0) this.splice(index, 1);
-}
+Object.defineProperty(Array.prototype, 'intersect', {
+    enumerable: false,
+    value: function(...args) {
+        return this.filter(val => {
+            for (let i = 0; i < args.length; i++) {
+                if (args[i].includes(val)) {
+                    return true;
+                }
+            }
+            
+            return false;
+        });
+    }
+});
 
-Array.prototype.pluck = function(prop, keyProp) {
-    if (typeof keyProp === 'undefined')
-        return this.map(elem => elem[prop]);
+Object.defineProperty(Array.prototype, 'remove', {
+    enumerable: false,
+    value: function(element) {
+        const index = this.indexOf(element);
+        if (index >= 0) this.splice(index, 1);
+    }
+});
 
-    let result = {};
-    this.forEach(elem => {
-        result[elem[keyProp]] = elem[prop];
-    });
-    return result;
-}
+Object.defineProperty(Array.prototype, 'pluck', {
+    enumerable: false,
+    value: function(prop, keyProp) {
+        if (typeof keyProp === 'undefined')
+            return this.map(elem => elem[prop]);
 
-Array.prototype.keyBy = function(keyProp) {
-    let result = {};
-    this.forEach(elem => {
-        result[elem[keyProp]] = elem;
-    });
-    return result;
-}
+        let result = {};
+        this.forEach(elem => {
+            result[elem[keyProp]] = elem[prop];
+        });
+        return result;
+    }
+});
+
+Object.defineProperty(Array.prototype, 'keyBy', {
+    enumerable: false,
+    value: function(keyProp) {
+        let result = {};
+        this.forEach(elem => {
+            result[elem[keyProp]] = elem;
+        });
+        return result;
+    }
+});
